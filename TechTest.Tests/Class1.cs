f579030050b61824820b15ca77dd4ec8123a1b64
Craig Godden-Payne <craig@goddenpayne.co.uk>
2019-01-25 14:00:50 +0000

    added test project

---
+++ TechTest.Tests/Class1.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace TechTest.Tests
+{
+    public class Class1
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.Pass();
+        }
+    }
+}
